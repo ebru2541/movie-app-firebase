49fe32d28dfa4924c48aed29d25ddfa28de48eeb
Rose <ebrue7949@gmail.com>
2023-03-15 18:29:03 +0300

    auth

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,15 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+//* https://firebase.google.com/docs/auth/web/start
+//* https://console.firebase.google.com/ => project settings
+//! firebase console settings bölümünden firebaseconfig ayarlarını al
 // TODO: Replace the following with your app's Firebase project configuration
-
 const firebaseConfig = {
-  //    apiKey: "AIzaSyC94LdQnYMbfsbCoyg4VP2GuC4yhY2PoQk",
-  //   authDomain: "movie-app-cfe3c.firebaseapp.com",
-  //   projectId: "movie-app-cfe3c",
-  //   storageBucket: "movie-app-cfe3c.appspot.com",
-  //   messagingSenderId: "125040196160",
-  //   appId: "1:125040196160:web:4f949a935e66b408c07da5"
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
